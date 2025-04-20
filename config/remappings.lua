faa928d61ccc44c98ce2e1e7c98c68f0310b7c4f
Ben Jackson <ben.jackson@bjss.com>
2025-04-20 08:21:50 +0100

    Add telescope

--- config/remappings.lua
+++ config/remappings.lua
@@ -9,3 +9,11 @@ map('n', '<leader>q', [[<CMD>q<CR>]], 'Quit')
 map('n', '<leader>Q', [[<CMD>qa!<CR>]], 'Force quit')
 map('n', '<leader>w', [[<CMD>w<CR>]], 'Write')
 
+-- telescope remappings
+local builtin = require('telescope.builtin')
+
+map('n', '<leader>ff', builtin.find_files, 'Telescope find files')
+map('n', '<leader>fg', builtin.live_grep, 'Telescope live grep')
+map('n', '<leader>fb', builtin.buffers, 'Telescope buffers')
+map('n', '<leader>fh', builtin.help_tags, 'Telescope help tags')
+
