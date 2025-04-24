444177c2e6d037dd6b8d2263f7b7e1dae70bbf3f
Ben Jackson <ben.jackson@bjss.com>
2025-04-24 12:44:32 +0100

    updates

--- lua/utl/mapper.lua
+++ lua/utl/mapper.lua
@@ -1,11 +1,10 @@
-local which_key = require('which-key')
+local which_key = require("which-key")
 
-return function(mapper_opts) 
-  return function(mode, key, cmd, desc) 
-    vim.keymap.set(mode, key, cmd, mapper_opts)
-    if desc then
-      which_key.add({ { key, desc = desc } })
-    end
-  end
+return function(mapper_opts)
+	return function(mode, key, cmd, desc)
+		vim.keymap.set(mode, key, cmd, mapper_opts)
+		if desc then
+			which_key.add({ { key, desc = desc } })
+		end
+	end
 end
-
